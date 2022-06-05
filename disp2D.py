bb720675040a9af1616e1eb3ec29011d9105fef8
Hiroaki Hata <hata@qc5.so-net.ne.jp>
2022-06-05 22:42:12 +0900

    20220605

--- disp2D.py
+++ disp2D.py
@@ -3,12 +3,38 @@ import dataload
 import numpy as np
 
 
+def disp3D(xx, yy, zz):
+    fig = plt.figure(figsize=(8, 8))  # unit inch
+
+    # contour
+    ax1 = fig.add_subplot(221, facecolor="w")
+    ax1.set_aspect("equal")
+    ax1.set_title("contour")
+    ax1.contour(xx, yy, zz, np.arange(0.01, 0.14, 0.01))
+
+    # surface
+    ax2 = fig.add_subplot(222, projection="3d", facecolor="w")
+    ax2.set_title("surface")
+    # ax2.plot_surface(xx, yy, zz, rstride=1, cstride=1, cmap=cm.coolwarm)
+    ax2.plot_surface(xx, yy, zz, rstride=1, cstride=1, cmap="plasma")
+    # ax2.plot_surface(xx, yy, zz, rstride=1, cstride=1, color="green")
+
+    # fireframe
+    ax3 = fig.add_subplot(223, projection="3d", facecolor="w")
+    ax3.set_title("wireframe")
+    ax3.plot_wireframe(
+        xx, yy, zz, linewidth=0.5, color="green",
+    )
+
+    plt.show()
+
+
 def boundary(range, classifier, labels, values=[0]):
     x = np.arange(range[0], range[1], 0.1)
     y = np.arange(range[2], range[3], 0.1)
     xx, yy = np.meshgrid(x, y)
-    xxx = xx.ravel()
-    yyy = yy.ravel()
+    # xxx = xx.ravel()
+    # yyy = yy.ravel()
     zz = np.array(classifier(xx, yy))
     zz.reshape(x.shape)
 
@@ -19,6 +45,11 @@ def disp2D1(data, labels):
             plt.plot(data[i][0], data[i][1], "g+")
         else:
             plt.plot(data[i][0], data[i][1], "r*")
+    plt.show()
+
+
+def plot2D(x, y):
+    plt.plot(x, y)
     plt.show()
 
 
@@ -34,6 +65,5 @@ def disp2D2(data1, data2, labels):
 
 
 if __name__ == "__main__":
-    c1, c2, l = dataload.load2D("points_ring.pkl")
-    disp2D2(c1, c2, l)
-
+    c1, c2, lbl = dataload.load2D("points_ring.pkl")
+    disp2D2(c1, c2, lbl)
--- test2DGauss.py
+++ test2DGauss.py
@@ -1,10 +1,34 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-# from matplotlib import cm
+import disp2D
 
 
-# 2D Gauss Distribution Function
+# 1th-D Gauss Distribution Function
+def gauss1D(x, m, v):
+    X = (x - m) ** 2
+    XX = -X / v
+    XXX = np.exp(XX / 2.0)
+    denom = np.sqrt(2 * np.pi * v + 1e-9)  # add small to denom. for safty
+    y = XXX / denom
+    return y
+
+
+# Nth-D Gauss Distribution Function
+def gaussN(p, m, v):
+    det = np.linalg.det(v)
+    try:
+        inv = np.linalg.inv(v)
+    except np.linalg.LinAlgError:
+        return np.zeros(0)
+    n = v.shape[0]
+    X = (p - m) @ inv @ (p - m).T
+    XX = -np.diag(X)
+    XXX = np.exp(XX / 2.0)
+    # add small to denom. for safty
+    denom = np.sqrt((2 * np.pi) ** n * det + 1e-9)
+    y = XXX / denom
+    return y
+
+
 """
 x: 1 Dimension List of X, len=N
 y: 1 Dimension List of Y, len=N
@@ -12,62 +36,42 @@ p[i]=(x[i],y[i])  0<=i<N
 m: mean point [mean of x, mean of y]
 s: covariance marix ( list of 2x2 dimension)
    s[0][0] varin
-
 """
 
 
-def disp3D(xx, yy, zz):
-    fig = plt.figure(figsize=(15, 5))  # unit inch
-
-    # contour
-    ax1 = fig.add_subplot(131, facecolor="w")
-    ax1.set_aspect("equal")
-    ax1.set_title("contour")
-    ax1.contour(xx, yy, zz, np.arange(0.01, 0.14, 0.01))
-
-    # surface
-    ax2 = fig.add_subplot(132, projection="3d", facecolor="w")
-    ax2.set_title("surface")
-    # ax2.plot_surface(xx, yy, zz, rstride=1, cstride=1, cmap=cm.coolwarm)
-    ax2.plot_surface(xx, yy, zz, rstride=1, cstride=1, cmap="plasma")
-    # ax2.plot_surface(xx, yy, zz, rstride=1, cstride=1, color="green")
-
-    # fireframe
-    ax3 = fig.add_subplot(133, projection="3d", facecolor="w")
-    ax3.set_title("wireframe")
-    ax3.plot_wireframe(
-        xx, yy, zz, linewidth=0.5, color="green",
-    )
-
-    plt.show()
+def gauss(p, m, v):
+    if type(v) is not np.ndarray:
+        return gauss1D(p, m, v)
+    else:
+        return gaussN(p, m, v)
 
 
-def gauss2D(p, m, v):
-    det = np.linalg.det(v)
-    try:
-        inv = np.linalg.inv(v)
-    except np.linalg.LinAlgError:
-        return np.zeros(0)
-    X = (p - m) @ inv @ (p - m).T
-    XX = -np.diag(X)
-    XXX = np.exp(XX / 2.0)
-    denom = np.sqrt((2 * np.pi) ** 2 * det + 1e-9)  # add small to denom. for safe
-    y = XXX / denom
-    return y
+def test2D():
+    # range of calclation
+    x = y = np.arange(-4, 4, 0.2)  # 40x40 points mesh
+    xx, yy = np.meshgrid(x, y)
+    xxx = xx.flatten()
+    yyy = yy.flatten()
+    p = np.c_[xxx, yyy]
+    m = np.array([0, 0])  # mean
+    v = np.array([[1, 0], [0, 4]])  # covariance matrix
+    zzz = gauss(p, m, v)
+    if zzz.shape[0] != p.shape[0]:
+        print("Error")
+        exit(0)
+    zz = zzz.reshape(xx.shape)
+    disp2D.disp3D(xx, yy, zz)
+    return  # for break point
 
 
-# range of calclation
-x = y = np.arange(-4, 4, 0.2)  # 40x40 points mesh
-xx, yy = np.meshgrid(x, y)
-xxx = xx.flatten()
-yyy = yy.flatten()
-p = np.c_[xxx, yyy]
-m = np.array([0, 0])  # mean
-v = np.array([[1, 0.3], [0.3, 2]])  # covariance matrix
-zzz = gauss2D(p, m, v)
-if zzz.shape[0] != p.shape[0]:
-    print("Error")
+def test1D():
+    x = np.arange(-5, 5, 0.2)  # 40points
+    m = 0
+    v = 4
+    z = gauss(x, m, v)
+    disp2D.plot2D(x, z)
+
+
+if __name__ == "__main__":
+    test2D()
     exit(0)
-zz = zzz.reshape(xx.shape)
-disp3D(xx, yy, zz)
-
